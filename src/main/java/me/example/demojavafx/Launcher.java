e5ac99a002de0ef1a177814a05245995b037d335
luigi <lu.differente@studenti.unina.it>
2025-11-12 16:58:11 +0100

    modify

--- src/main/java/me/example/demojavafx/Launcher.java
+++ src/main/java/me/example/demojavafx/Launcher.java
@@ -1,6 +1,7 @@
 package me.example.demojavafx;
 
 import javafx.application.Application;
+import me.example.demojavafx.frontend.HelloApplication;
 
 public class Launcher {
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module me.example.demojavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens me.example.demojavafx.frontend to javafx.fxml;
 
-    opens me.example.demojavafx to javafx.fxml;
     exports me.example.demojavafx;
+    exports me.example.demojavafx.frontend;
+    //exports me.example.demojavafx.beckend;
 }
